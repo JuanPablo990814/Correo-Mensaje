af76d943ccc22df8d566d7fcdfff2fd43f0465d4
juanpablo990814 <juanpablopb99@gmail.com>
2021-05-12 02:23:30 -0500

    Primer Commit

---
+++ Mensaje/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Mensaje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.txtPara = new System.Windows.Forms.TextBox();
+            this.txtBcc = new System.Windows.Forms.TextBox();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAdjuntar = new System.Windows.Forms.Button();
+            this.txtAdjunto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(700, 411);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(29, 169);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(746, 211);
+            this.txtMensaje.TabIndex = 1;
+            // 
+            // txtPara
+            // 
+            this.txtPara.Location = new System.Drawing.Point(29, 69);
+            this.txtPara.Name = "txtPara";
+            this.txtPara.Size = new System.Drawing.Size(298, 20);
+            this.txtPara.TabIndex = 2;
+            // 
+            // txtBcc
+            // 
+            this.txtBcc.Location = new System.Drawing.Point(455, 69);
+            this.txtBcc.Name = "txtBcc";
+            this.txtBcc.Size = new System.Drawing.Size(320, 20);
+            this.txtBcc.TabIndex = 3;
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(29, 117);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(298, 20);
+            this.txtAsunto.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Para:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Asunto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bcc:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Mensaje:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(295, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Mensaje al Correo";
+            // 
+            // btnAdjuntar
+            // 
+            this.btnAdjuntar.Location = new System.Drawing.Point(700, 140);
+            this.btnAdjuntar.Name = "btnAdjuntar";
+            this.btnAdjuntar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdjuntar.TabIndex = 10;
+            this.btnAdjuntar.Text = "Adjuntar";
+            this.btnAdjuntar.UseVisualStyleBackColor = true;
+            this.btnAdjuntar.Click += new System.EventHandler(this.btnAdjuntar_Click);
+            // 
+            // txtAdjunto
+            // 
+            this.txtAdjunto.Enabled = false;
+            this.txtAdjunto.Location = new System.Drawing.Point(594, 142);
+            this.txtAdjunto.Name = "txtAdjunto";
+            this.txtAdjunto.Size = new System.Drawing.Size(100, 20);
+            this.txtAdjunto.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAdjunto);
+            this.Controls.Add(this.btnAdjuntar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.txtBcc);
+            this.Controls.Add(this.txtPara);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "Form1";
+            this.Text = "Mensaje";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.TextBox txtPara;
+        private System.Windows.Forms.TextBox txtBcc;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdjuntar;
+        private System.Windows.Forms.TextBox txtAdjunto;
+    }
+}
+
---
+++ Mensaje/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mensaje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+        System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            
+            mmsg.To.Add(txtPara.Text);
+            mmsg.Subject = txtAsunto.Text;
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+            mmsg.Bcc.Add(txtBcc.Text);
+            
+
+            mmsg.Body = txtMensaje.Text;
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = true;
+
+            if (txtAdjunto.Text != (""))
+            {
+                System.Net.Mail.Attachment archivo = new System.Net.Mail.Attachment(txtAdjunto.Text);
+                mmsg.Attachments.Add(archivo);
+            }
+
+            //GMAIL
+            //mmsg.From = new System.Net.Mail.MailAddress("juanppatii11@gmail.com");
+
+            //HOTMAIL
+            mmsg.From = new System.Net.Mail.MailAddress("JPablopb_14@hotmail.com");
+
+            //Correo Cliente
+
+            //GMAIL
+            //cliente.Credentials = new System.Net.NetworkCredential("juanppatii11@gmail.com", "contraseñacorrreo");
+
+            //HOTMAIL
+            cliente.Credentials = new System.Net.NetworkCredential("JPablopb_14@hotmail.com", "contraseñacorreo");
+
+            cliente.Port = 587;
+            cliente.EnableSsl = true;
+
+            //GMAIL
+            //cliente.Host = "smtp.gmail.com"; //mail.dominio.com
+            
+            //HOTMAIL
+            cliente.Host = "smtp.live.com";
+
+            try
+            {
+                cliente.Send(mmsg);
+                MessageBox.Show("Mensaje Enviado");
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Error al Enviar");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdjuntar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Adjuntar = new OpenFileDialog();
+            Adjuntar.ShowDialog();
+            if(Adjuntar.FileName != (""))
+            {
+                txtAdjunto.Text = Adjuntar.FileName;
+            }
+        }
+    }
+}
